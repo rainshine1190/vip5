cdd74c1ada49b52e32284c59c82b25b88948c3c2
rainshine1190 <rainshine1190@126.com>
2020-02-29 18:44:12 +0800

    python基础练习

---
+++ mytest.py
@@ -0,0 +1,62 @@
+
+#a = 2
+'''
+
+b = 2.5
+c = 'happy'
+d = "let's go"
+
+'''
+a = 2
+b = 2.5
+c = 'happy'
+d = "let's go"
+
+# print(a,b,c,d)
+
+# #为变量e从键盘接收数值
+# e = input('请输入你的数值：')
+# print(e)
+
+#为两个变量输入
+# f,g = input('请为两个数输入，并用逗号分隔：').split(',')
+# print('f和g的指为：',f,g,type(f),type(g))
+
+# #格式化输出
+# print(('%d,%f,%s')%(a,b,c))
+#
+# #format输出
+# print(('a={},b={},c={}').format(a,b,c))
+
+
+#列表
+# a = [1,2,3,4,5,6,7,8,9]
+#
+# print(a[2:7:2])
+#
+# print(a[:5])
+
+# a='5'
+# print(int(a))
+#
+# print(list(range(5)))
+# print(list(range(1,5)))
+# print(list(range(1,9,2)))
+
+
+# a = [5,2,3,9,1,6,7,8,4]
+# # a.reverse()
+# # a.sort()
+#
+# result = sorted(a)
+# re = a.reverse()
+#
+# print('re',re)
+# print('result',result)
+# print(a)
+
+a = 2
+
+a*=3+3
+
+print(a)
